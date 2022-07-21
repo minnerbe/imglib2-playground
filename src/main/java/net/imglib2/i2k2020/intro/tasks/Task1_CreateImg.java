8d6fa9fac558e07ff8a0acc27930ab1b58fc8693
Michael Innerberger <michael.innerberger@asc.tuwien.ac.at>
2022-07-21 12:38:57 +0200

    Add solution to Task1

--- src/main/java/net/imglib2/i2k2020/intro/tasks/Task1_CreateImg.java
+++ src/main/java/net/imglib2/i2k2020/intro/tasks/Task1_CreateImg.java
@@ -2,6 +2,13 @@ package net.imglib2.i2k2020.intro.tasks;
 
 import net.imglib2.img.Img;
 import net.imglib2.type.numeric.real.FloatType;
+import net.imglib2.Cursor;
+import net.imglib2.RandomAccess;
+import net.imglib2.img.ImgFactory;
+import net.imglib2.img.array.ArrayImgs;
+import net.imglib2.img.array.ArrayImgFactory;
+
+import java.util.Random;
 
 /**
  * Illustrates how to use an array in ImgLib2, how to use iteration, random
@@ -21,19 +28,26 @@ public class Task1_CreateImg {
 	 */
 	public static Img<FloatType> createNewImg() {
 
-		//TODO: fill
-
 		// create a 5x5 pixel image of type float
+		final ImgFactory<FloatType> imgFactory = new ArrayImgFactory<FloatType>(new FloatType());
+		final Img<FloatType> img = imgFactory.create(5, 5);
 
 		// generate a random number generator
+		Random rnd = new Random(System.currentTimeMillis());
 
 		// iterate all pixels of that image and set it to a random number
+		for(FloatType pixel: img) {
+			pixel.set(rnd.nextFloat());
+		}
 
-		// print out all pixel values together with their position using a
-		// Cursor
+		printImageWithPositions(img);
+		System.out.println("");
 
 		// random access to a central pixel
-
+		RandomAccess<FloatType> randomAccess = img.randomAccess();
+		FloatType value = randomAccess.setPositionAndGet(2, 2);
+		System.out.format("central pixel @ (2,2) = %f.\n", value.get());
+		
 		return null;
 	}
 
@@ -45,9 +59,34 @@ public class Task1_CreateImg {
 	 */
 	public static Img<FloatType> createImgFromArray() {
 
-		//TODO: fill
+		final float[] array = new float[5 * 5];
 
-		return null;
+		for (int i = 0; i < array.length; ++i) {
+			array[i] = i;
+		}
+
+		final Img<FloatType> img = ArrayImgs.floats(array, 5, 5);
+
+		printImageWithPositions(img);
+
+		return img;
+	}
+	
+	/**
+	 * Print out all pixel values together with their position using a
+	 * Cursor.
+	 *
+	 * @ param img Image to be printed.
+	 * 		
+	 */
+	public static void printImageWithPositions(Img img) {
+		Cursor<FloatType> cursor = img.localizingCursor();
+		int[] position = new int[2];
+		while(cursor.hasNext()) {
+			cursor.fwd();
+			cursor.localize(position);
+			System.out.format("img(%d,%d) = %f.\n", position[0], position[1], cursor.get().get());
+		}
 	}
 
 	public static void main(String[] args) {
