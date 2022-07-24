332bf7fa4f2c99b2d4e406fb6bc004d22e338101
Michael Innerberger <michael.innerberger@asc.tuwien.ac.at>
2022-07-24 19:14:51 +0200

    Convert pictures to .pgm to get only one channel

--- src/main/java/net/imglib2/i2k2020/intro/tasks/OwnTask1_PdeDenoiser.java
+++ src/main/java/net/imglib2/i2k2020/intro/tasks/OwnTask1_PdeDenoiser.java
@@ -6,9 +6,10 @@ import net.imglib2.RandomAccessible;
 import net.imglib2.RandomAccess;
 import net.imglib2.Cursor;
 import net.imglib2.view.Views;
-import net.imglib2.type.numeric.real.FloatType;
-import net.imglib2.type.numeric.NumericType;
 import net.imglib2.type.NativeType;
+import net.imglib2.type.numeric.NumericType;
+import net.imglib2.type.numeric.RealType;
+import net.imglib2.type.numeric.real.FloatType;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.img.imageplus.ImagePlusImgs;
 
@@ -167,16 +168,11 @@ public class OwnTask1_PdeDenoiser {
 		}
 	}
 
-	/**
-	 * Use ArrayImg to create a 5x5 float image and fill with random numbers,
-	 *
-	 * @return
-	 */
-	public static <T extends NumericType<T> & NativeType<T>> void main(String[] args) {
+	public static <T extends RealType<T> & NativeType<T>> void main(String[] args) {
 		// open grayscale image
-		final String imgLocation = "/home/michael/data/Programming/Java/imglib2-intro/pictures/noisy-image.png";
-		// final String imgLocation = "/home/michael/data/Programming/Java/imglib2-intro/pictures/plane-with-noise.png";
-		final Img<T> img = ImagePlusImgs.from(IJ.openImage(imgLocation));
+		final String imgLocation = "/home/michael/data/Programming/Java/imglib2-intro/pictures/noisy-image.pgm";
+		// final String imgLocation = "/home/michael/data/Programming/Java/imglib2-intro/pictures/plane-with-noise.pgm";
+		Img<T> img = ImagePlusImgs.from(IJ.openImage(imgLocation));
 		ImageJFunctions.show(img);
 
 		denoise(img, 10);
