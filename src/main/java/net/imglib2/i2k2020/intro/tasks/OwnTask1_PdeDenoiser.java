b3926941d86e95d086c73f7c225154620bdda6f1
Michael Innerberger <michael.innerberger@asc.tuwien.ac.at>
2022-07-26 09:02:38 +0200

    Remove old implementation and add documentation

--- src/main/java/net/imglib2/i2k2020/intro/tasks/OwnTask1_PdeDenoiser.java
+++ src/main/java/net/imglib2/i2k2020/intro/tasks/OwnTask1_PdeDenoiser.java
@@ -30,149 +30,6 @@ import java.lang.reflect.Array;
  */
 public class OwnTask1_PdeDenoiser {
 
-	/**
-	 * Compute finite differences in d-th dimension.
-	 *
-	 * @param source	Source image.
-	 * @param target	Target image.
-	 * @param d			Dimension along which differences are to be computed.
-	 */
-	private static <T extends NumericType<T> & NativeType<T>> void computeFiniteDifferences(Img<T> source, Img<T> target, int d) {
-		final RandomAccessible<T> infiniteSource = Views.extendMirrorSingle(source);
-		final RandomAccess<T> randomAccess = infiniteSource.randomAccess();
-		final Cursor<T> sourceCursor = source.localizingCursor();
-		final Cursor<T> targetCursor = target.localizingCursor();
-		final long[] position = new long[source.numDimensions()];
-		T diff = null;
-
-		while (sourceCursor.hasNext()) {
-			sourceCursor.fwd();
-			targetCursor.fwd();
-
-			// compute difference of pixel with position +1/-1 in dimension d
-			sourceCursor.localize(position);
-			position[d] += 1;
-			diff = randomAccess.setPositionAndGet(position).copy();
-			position[d] -= 2;
-			diff.sub(randomAccess.setPositionAndGet(position));
-
-			targetCursor.get().set(diff);
-		}
-	}
-
-	/**
-	 * Compute Perona-Malik flux 1/(1+|Du|^2/lambda^2) Du of discrete gradient.
-	 *
-	 * @param 	gradient	
-	 *
-	 * @return 	flux
-	 */
-	private static <T extends NumericType<T> & NativeType<T>> void computeFlux(Img<T>[] gradient) {
-		int n = gradient.length;
-		final Img<T> duSquared = gradient[0].factory().create(gradient[0]);
-		final Cursor<T> cursorDu = duSquared.cursor();
-		final Cursor<T>[] cursorComponent = (Cursor<T>[]) Array.newInstance(cursorDu.getClass(), n);
-
-		for (int i=0; i<n; ++i) {
-			cursorComponent[i] = gradient[i].cursor();
-		}
-
-		T val = null;
-		T component = null;
-		while (cursorDu.hasNext()) {
-			val = cursorDu.next();
-			val.setOne(); // TODO: introduce parameter lambda^2 here
-			for (int i=0; i<n; ++i) {
-				component = cursorComponent[i].next().copy();
-				component.mul(component);
-				val.add(component);
-			}
-			val.pow(-1);
-			for (int i=0; i<n; ++i) {
-				component.mul(val);
-				cursorComponent[i].get().set(component);
-			}
-		}
-	}
-
-	/**
-	 * Compute Perona-Malik denoising of image.
-	 *
-	 * @param 	img	
-	 * @param 	nTimeSteps	
-	 *
-	 * @return 	denoised image
-	 */
-	private static <T extends NumericType<T> & NativeType<T>> Img<T> denoise(Img<T> img, int nTimeSteps) {
-		int n = img.numDimensions();
-		final Img<T> denoisedImg = copyImage(img);
-		final Img<T>[] gradient = (Img<T>[]) Array.newInstance(img.getClass(), n);
-		final Img<T>[] flux = null;
-
-		for (int i=0; i<n; ++i) {
-			gradient[i] = img.factory().create(img);
-		}
-
-		for (int t=0; t<nTimeSteps; ++t) {
-			for (int i=0; i<n; ++i) {
-				computeFiniteDifferences(img, gradient[i], i);
-			}
-			computeFlux(gradient);
-			for (int i=0; i<n; ++i) {
-				computeFiniteDifferences(gradient[i], gradient[i], i);
-			}
-			addTimeStep(denoisedImg, gradient);
-		}
-
-		return denoisedImg;
-	}
-
-	/**
-	 * Generic, type-agnostic method to create an identical copy of an Img
-	 *
-	 * @param input - the Img to copy
-	 * @return - the copy of the Img
-	 */
-	private static <T extends NumericType<T> & NativeType<T>> Img<T> copyImage(final Img<T> input) {
-		Img<T> output = input.factory().create(input);
-		Cursor<T> cursorInput = input.cursor();
-		Cursor<T> cursorOutput = output.cursor();
- 
-		while (cursorInput.hasNext()) {
-			cursorInput.fwd();
-			cursorOutput.fwd();
-			cursorOutput.get().set(cursorInput.get());
-		}
- 
-		return output;
-	}
-
-	/**
-	 * Add one timestep to the image.
-	 *
-	 * @param 	img	
-	 * @param 	update	
-	 */
-	private static <T extends NumericType<T> & NativeType<T>> void addTimeStep(Img<T> img, Img<T>[] update) {
-		int n = update.length;
-		final Cursor<T> cursor = img.cursor();
-		final Cursor<T>[] cursorComponent = (Cursor<T>[]) Array.newInstance(cursor.getClass(), n);
-
-		for (int i=0; i<n; ++i) {
-			cursorComponent[i] = update[i].cursor();
-		}
-
-		T val = null;
-		T component = null;
-		while (cursor.hasNext()) {
-			val = cursor.next();
-			for (int i=0; i<n; ++i) {
-				component = cursorComponent[i].next();
-				val.add(component); // TODO: add step size
-			}
-		}
-	}
-
 	public <T extends RealType<T> & NativeType<T>> OwnTask1_PdeDenoiser() {
 		// open grayscale image
 		final String imgLocation = "/home/michael/data/Programming/Java/imglib2-intro/pictures/noisy-image.pgm";
@@ -180,12 +37,8 @@ public class OwnTask1_PdeDenoiser {
 		Img<T> img = ImagePlusImgs.from(IJ.openImage(imgLocation));
 		ImageJFunctions.show(img);
 
-		denoise(img, 10);
-
-		ImageJFunctions.show(img);
-
 		final PeronaMalikDenoiser pmd = new PeronaMalikDenoiser(1.0, 0.001);
-		Img<DoubleType> denoisedImg = pmd.denoise(img, 10);
+		Img<DoubleType> denoisedImg = pmd.denoise(img, 1000);
 		ImageJFunctions.show(denoisedImg);
 	}
 
@@ -193,6 +46,16 @@ public class OwnTask1_PdeDenoiser {
 		new OwnTask1_PdeDenoiser();
 	}
 
+	/**
+	 * Compute a denoised image using a PDE-based method suggested by Perona
+	 * and Malik: compute the solution to the model
+	 * 	d_t u = div(g(|grad u|^2) * grad u),
+	 * 	where the flux is g(s) = 1/(1+s/lambda).
+	 * The parameter alpha steers the step sizes (alpha = delta t / h) of the
+	 * time discretization.  The parameter beta controls the strength of the
+	 * diffusion (beta = lambda / h^2).  The spatial step size of the finite
+	 * difference scheme is implicitly given by h>0.
+	 */
 	private class PeronaMalikDenoiser {
 		private int numDimensions;
 		private DoubleType alpha;
@@ -261,6 +124,9 @@ public class OwnTask1_PdeDenoiser {
 			return imgFactory.create(dimensions);
 		}
 
+		/**
+		 * Compute discrete flux via finite differences.
+		 */
 		private void computeFlux() {
 			final Cursor<DoubleType> stateCursor = currentState.localizingCursor();
 			final Cursor<DoubleType>[] fluxCursor = (Cursor<DoubleType>[]) Array.newInstance(currentState.cursor().getClass(), numDimensions);
@@ -296,14 +162,19 @@ public class OwnTask1_PdeDenoiser {
 			}
 		}
 
-		// s -> 1/(1+s/beta)
+		/**
+		 * Perform nonlinear trasformation of gradient norm: s -> 1/(1+s/beta).
+		 */
 		private void transformDiffusionCoefficient(DoubleType s) {
 			s.div(beta);
 			s.add(one);
 			s.pow(-1);
 		}
 
-		// compute difference of pixel with position +1/-1 in specified dimension
+		/**
+		 * Compute finite differences in specified dimension (difference of
+		 * pixels with position +1/-1 in specified dimension).
+		 */
 		private void computeDirectionalDerivative(RandomAccess<DoubleType> access, Point position, int dimension, DoubleType result) {
 				position.move(1, dimension);
 				result.setReal(access.setPositionAndGet(position).getRealDouble());
@@ -312,6 +183,9 @@ public class OwnTask1_PdeDenoiser {
 				position.move(1, dimension);
 		}
 		
+		/**
+		 * Add one timestep to the image.
+		 */
 		private void updateState() {
 			final Cursor<DoubleType> stateCursor = currentState.localizingCursor();
 			final RandomAccess<DoubleType>[] fluxAccess =
